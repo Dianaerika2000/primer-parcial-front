6ccb7be880fee16c4a1a987d527d1f950c40f9b6
dianaM <dianaerika.montano16@gmail.com>
2024-01-23 14:51:55 -0400

    Change api url

--- src/API/axios.jsx
+++ src/API/axios.jsx
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  // baseURL: "https://diagram-api.adaptable.app/api",
-  baseURL: "http://localhost:3000/api",
+  baseURL: "https://diagram-api.adaptable.app/api",
+  // baseURL: "http://localhost:3000/api",
   headers: {
     Accept: "application/json",
     "Content-Type": "application/json",
